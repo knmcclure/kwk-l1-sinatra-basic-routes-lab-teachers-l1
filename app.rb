a77904df08fe585efe8dd7ffcdb001681da09dcc
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-06-17 01:46:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+get '/name' do
+  "My name is Kyla"
+end
+
+get '/hometown' do
+  "My hometown is Vista, CA"
+end
+
+get '/favorite-song' do
+  "My favorite song is Where is the love" 
+end
+  
+  
+  
 end
